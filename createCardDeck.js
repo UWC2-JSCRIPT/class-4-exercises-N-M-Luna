e9fab57fca4278dccd98b6b732232a5bab7993d3
N. M. Luna <nat.luna.64@gmail.com>
2022-11-09 15:09:47 -0800

    add getDeck function

--- createCardDeck.js
+++ createCardDeck.js
@@ -4,8 +4,56 @@
  */
 const getDeck = () => {
 
-}
+  // Create a 52-card deck of cards
+  const deck = []
+  let card
 
+  // For each of the suits,
+  const suits = ['hearts', 'spades', 'club', 'diamonds']
+  for (suit of suits) {
+
+    // For each of the 13 different cards,
+    for (var i = 2; i < 15; i++) {
+      
+      // Create a card object (with val, displayVal, and suit)
+      let value, display
+      switch (i) {
+        case 11:
+          display = 'Jack'
+          value = 10
+          break;
+        case 12:
+          display = 'Queen'
+          value = 10
+          break;
+        case 13:
+          display = 'King'
+          value = 10
+        break;
+        case 14:
+          display = 'Ace'
+          value = 11
+        break;
+        default:
+          display = i.toString()
+          value = i
+        break;
+      }
+    
+      card = {
+        val: value,
+        displayVal: display,
+        suit: suit
+      }
+
+      //Push the new card in the deck we're building
+        deck.push(card)
+        console.log(card)
+      }
+    }
+    
+    return deck;
+  }
 
 
 // CHECKS
