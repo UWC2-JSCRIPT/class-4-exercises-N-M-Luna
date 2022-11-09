c3f31f1aa811ee1beee4be0f8876bf7e7bb4bdc8
N. M. Luna <nat.luna.64@gmail.com>
2022-11-09 14:35:13 -0800

    add foodIsCooked function

--- foodIsCooked.js
+++ foodIsCooked.js
@@ -6,8 +6,28 @@
  * @returns {boolean} isCooked
  */
 const foodIsCooked = function(kind, internalTemp, doneness) {
-  // Write function HERE
-
+  // If it's beef, the temp depends on the doneness
+  if (kind === 'beef') {
+    switch (doneness) {
+      case 'rare':
+        // Temp for rare should be above 125 but below 135
+        return internalTemp > 125 && internalTemp <= 135
+        break;
+        case 'medium':
+          // Temp for medium should be above 135 but below 155
+          return internalTemp > 135 && internalTemp <= 155
+        break;
+        case 'well':
+          //Temp for well done should be above 155
+          return internalTemp > 155 
+        break;
+      default: // Chef is confused by request
+      return null
+        break;
+    }
+  } else { //If it's chicken, temp should be above 165
+  return internalTemp > 165 
+}
 }
 
 
